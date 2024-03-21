ef4a8d9e2ee16d3bac2ba87836c37fb6d73e4f40
jakebag <jakecwilloughby@gmail.com>
2024-03-21 18:59:04 -0400

    ready for render

--- db/client.js
+++ db/client.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg")
 require("dotenv").config()
 
-let DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:5432/luigis_pizzeria"
+let DATABASE_URL = process.env.DATABASE_URL
+// console.log(DATABASE_URL)
 
 // console.log(DATABASE_URL)
 const client = new Client({ connectionString: DATABASE_URL, ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined, })
